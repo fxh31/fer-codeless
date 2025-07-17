12731ed5227393b3e9a7d335a814cea3872d6d09
dawn <1194815021@qq.com>
2025-07-17 16:29:46 +0800

    docs: BasicTitle

--- packages/designer/src/components/Basic/index.ts
+++ packages/designer/src/components/Basic/index.ts
@@ -1,4 +1,6 @@
 import { withInstall } from '@fer-codeless/utils';
 import basicHelp from './src/BasicHelp.vue';
+import basicTitle from './src/BasicTitle.vue';
 
 export const BasicHelp = withInstall(basicHelp);
+export const BasicTitle = withInstall(basicTitle);
