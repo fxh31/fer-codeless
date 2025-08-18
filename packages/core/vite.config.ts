3963b4eb99977ab3dbacc556a060a12a2489f79d
dawn <1194815021@qq.com>
2025-08-18 17:55:17 +0800

    chore: 添加 git 工作流，自动部署

--- packages/core/vite.config.ts
+++ packages/core/vite.config.ts
@@ -5,6 +5,10 @@ import { fileURLToPath, URL } from 'node:url';
 import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
 
+function pathResolve(dir: string) {
+  return resolve(process.cwd(), '.', dir);
+}
+
 export default defineConfig({
   plugins: [vue(), vueJsx()],
   resolve: {
@@ -21,6 +25,21 @@ export default defineConfig({
           hack: `true; @import (reference) "${resolve('src/design/config.less')}";`,
           'selected-hover-bg': '#f5f5f5',
         },
+      },
+    },
+  },
+  build: {
+    target: 'esnext',
+    minify: true,
+    rollupOptions: {
+      input: {
+        index: pathResolve('index.html'),
+      },
+      // 静态资源分类打包
+      output: {
+        chunkFileNames: 'static/js/[name]-[hash].js',
+        entryFileNames: 'static/js/[name]-[hash].js',
+        assetFileNames: 'static/[ext]/[name]-[hash].[ext]',
       },
     },
   },
